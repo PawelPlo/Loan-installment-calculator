b21cd95a7e24eb3452fd113fc8fb5aeb4faf16d8
Pawel Plociennik <p.plociennik@wp.pl>
2023-04-19 19:39:45 +0200

    Data requests added

---
+++ main.py
@@ -0,0 +1,12 @@
+#Loan installment calculator
+
+kwota_kredytu = input("Wpisz wysokość kredytu w PLN:   ")
+print(f"Wysokość kredytu to {kwota_kredytu} PLN")
+
+oprocentowanie_kredytu = input("Wpisz wysokość oprocentowania:   ")
+print(f"Wysokość oprocentowania wynosi {oprocentowanie_kredytu} %")
+
+wysokość_raty = input("Wpisz proponowaną wysokość raty w PLN:     ")
+print(f"Proponowana przez Ciebie wysokość raty to {wysokość_raty} PLN")
+
+
