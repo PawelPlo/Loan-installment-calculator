88124c6bd87aa26eb37b7994f739337925214fb4
Pawel Plociennik <p.plociennik@wp.pl>
2023-04-20 10:28:59 +0200

    First month capital mathematical formula modified

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #Loan installment calculator
 
 kwota_kredytu = input("Wpisz wysokość kredytu w PLN:   ")
-kwota_kredytu = int(kwota_kredytu)
+kwota_kredytu = float(kwota_kredytu)
 print(f"Wysokość kredytu to {kwota_kredytu} PLN")
 
 oprocentowanie_kredytu = input("Wpisz wysokość oprocentowania:   ")
-oprocentowanie_kredytu = int(oprocentowanie_kredytu)
+oprocentowanie_kredytu = float(oprocentowanie_kredytu)
 print(f"Wysokość oprocentowania wynosi {oprocentowanie_kredytu} %")
 
 wysokość_raty = input("Wpisz proponowaną wysokość raty w PLN:     ")
-wysokość_raty= int(wysokość_raty)
+wysokość_raty= float(wysokość_raty)
 print(f"Proponowana przez Ciebie wysokość raty to {wysokość_raty} PLN")
 
-
-
+#Wyliczenia dla pierwszego miesiąca
 
 inf_1=1.592824484
 inf_1=float(inf_1)
 kapitał_1=(1+(inf_1 + oprocentowanie_kredytu)/1200)*kwota_kredytu-wysokość_raty
-print(kapitał_1)
+pomniejszenie_kapitału=kwota_kredytu-kapitał_1
+print(f"Po pierwszym miesiącu Twoja pozostała kwota kredytu to {kapitał_1} PLN,\n to o {pomniejszenie_kapitału} PLN mniej niż w poprzednim miesiącu")
